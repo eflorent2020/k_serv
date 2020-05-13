953dcc87705924348ffd073376ff57da44a6c0ab
estellefay <estelle-fayolle@hotmail.fr>
2020-05-13 15:59:56 +0200

    test get

--- api/controller/index.py
+++ api/controller/index.py
@@ -4,3 +4,7 @@ from flask import jsonify
 @app.route('/')
 def hello_world():
     return jsonify({"message": "hello world"})
+
+@app.route('/signboard/:customer_code/:signboard_code')
+def signboard():
+    return jsonify({"message": "good"})

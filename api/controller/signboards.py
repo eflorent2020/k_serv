32a619ffff0f9b6d88a98ed5889ea56ab2449a19
Nephenee <53172436+Nephenee@users.noreply.github.com>
2020-05-13 14:39:49 +0200

    Update signboards.py

--- api/controller/signboards.py
+++ api/controller/signboards.py
@@ -6,7 +6,3 @@ from api.model.signboard import Signboard
 def get_signboard():
     r = Signboard.query.all()
     return jsonify({"data": r})
-
-def del_signboard(id):
-  Signboard.delete(id)
-  Signboard.commit
